40ee978db51c01ced872a00909969a00c2a6ed14
DylanJQ <47297648+Dylan-J-Q@users.noreply.github.com>
2023-11-13 18:32:39 +0000

    update

--- src/main/java/UoBToolchainGroup/DistributedToolchainIntegration/WebController.java
+++ src/main/java/UoBToolchainGroup/DistributedToolchainIntegration/WebController.java
@@ -1,14 +1,16 @@
 package UoBToolchainGroup.DistributedToolchainIntegration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebController {
 
-    @GetMapping(value = "/hello")
-    @ResponseBody
-    public String index(){
-        return "Hello World!";
+    @GetMapping(value = "/")
+    public String index(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+                        Model model){
+        model.addAttribute("greeting", "Hello " + name);
+        return "index";
     }
 }
