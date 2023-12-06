90e56006218129e89381fd3f79a168b4dbd151b0
georgecorfield <gacorfield50@gmail.com>
2023-12-06 18:01:36 +0000

    update to controller directories

--- src/main/java/UoBToolchainGroup/DistributedToolchainIntegration/controller/WebController.java
+++ src/main/java/UoBToolchainGroup/DistributedToolchainIntegration/controller/WebController.java
@@ -1,4 +1,4 @@
-package UoBToolchainGroup.controller;
+package UoBToolchainGroup.DistributedToolchainIntegration.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
