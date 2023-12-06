981a33306b3c0e7ae0e3fa2c17bdf0dded515f23
DylanJQ <47297648+Dylan-J-Q@users.noreply.github.com>
2023-12-06 19:51:01 +0000

    Removal of unecessary comment
    title.

--- src/testPython/test.py
+++ src/testPython/test.py
@@ -1,26 +1,6 @@
 # used for unit testing 
 # to run a test use while in the python directory:
 #   py -m unittest
-#
-# preferably this gets moved to the test folder, but
-# when moved to the test folder it cant discover testModule and the following error is returned
-#
-#ImportError: Failed to import test module: test
-#Traceback (most recent call last):
-#  File "C:\Users\dylan\AppData\Local\Programs\Python\Python310\lib\unittest\loader.py", line 436, in _find_e 436, in _find_test_path
-#    module = self._get_module_from_name(name)
-#  File "C:\Users\dylan\AppData\Local\Programs\Python\Python310\lib\unittest\loader.py", line 377, in _get_me 377, in _get_module_from_name
-#    __import__(name)
-#  File "S:\Uni-Coursework\2023-DistributedToolchainIntegration\src\test\java\UoBToolchainGroup\DistributedToup\DistributedToolchainIntegration\test.py", line 8, in <module>
-#    import testModule
-#ModuleNotFoundError: No module named 'testModule'
-#
-# I have tried the following
-# adding an __init__.py file to both the test folder and the python folder.
-# attempting to modify the PYTHONPATH variable
-# attempting to use code to set the path
-#
-# Any other ideas would be appreciated.
 
 import unittest, sys
 
