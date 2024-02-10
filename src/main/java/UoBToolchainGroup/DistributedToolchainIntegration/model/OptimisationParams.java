2a1b83a985ce4d63a1df36aeb6247c5857b0ae42
georgecorfield <gacorfield50@gmail.com>
2024-02-10 20:20:29 +0000

    refactored Optimisation Parameters

--- src/main/java/UoBToolchainGroup/DistributedToolchainIntegration/model/OptimisationParams.java
+++ src/main/java/UoBToolchainGroup/DistributedToolchainIntegration/model/OptimisationParams.java
@@ -9,18 +9,15 @@ public class OptimisationParams {
 
     @Id
     private String paramsId;
-    private Project project;
-    private CadFile cadFile;
+    //add other variables here
 
     public OptimisationParams(){
         super();
     }
 
-    public OptimisationParams(String paramsId, Project project, CadFile cadFile){
+    public OptimisationParams(String paramsId){
         super();
         this.paramsId = paramsId;
-        this.project = project;
-        this.cadFile = cadFile;
     }
     
     public String getParamsId(){
@@ -29,22 +26,6 @@ public class OptimisationParams {
 
     public void setParamsId(String paramsId){
         this.paramsId = paramsId;
-    }
-
-    public Project getProject(){
-        return project;
-    }
-
-    public void setProject(Project project){
-        this.project = project;
-    }
-
-    public CadFile getCadFile(){
-        return cadFile;
-    }
-
-    public void setCadFile(CadFile cadFile){
-        this.cadFile = cadFile;
     }
 
 
