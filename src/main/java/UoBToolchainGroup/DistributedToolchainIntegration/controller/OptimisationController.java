da3f109bda9b6688301ad3b7f0569b22e73ee122
georgecorfield <gacorfield50@gmail.com>
2024-03-19 01:58:49 +0000

    Added form to change optimisation parameters

--- src/main/java/UoBToolchainGroup/DistributedToolchainIntegration/controller/OptimisationController.java
+++ src/main/java/UoBToolchainGroup/DistributedToolchainIntegration/controller/OptimisationController.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import UoBToolchainGroup.DistributedToolchainIntegration.model.OptimisationParams;
 import UoBToolchainGroup.DistributedToolchainIntegration.model.Part;
@@ -24,10 +26,21 @@ public class OptimisationController {
     public String getOptimisation(@PathVariable String projectName, @PathVariable String partId, Model model){
         Part part = partService.getPartbyId(new ObjectId(partId));
         OptimisationParams op = part.getOptimisationParams();
-        System.out.println("HERE");
-        System.out.println(op.getModules());
         model.addAttribute("part", part);
+        model.addAttribute("projectName", projectName);
         model.addAttribute("opParams", op);
+        model.addAttribute("newOp", new OptimisationParams());
+        return "optimise";
+    }
+
+    @PostMapping("/projects/{projectName}/{partId}/optimise")
+    public String setIterationLimit(@PathVariable String projectName, @PathVariable String partId, Model model, @ModelAttribute("newOp") OptimisationParams newOp){
+        // Part part = partService.getPartbyId(new ObjectId(partId));
+        // OptimisationParams op = part.getOptimisationParams();
+        // op.setIterations(newOp.getIterations());
+        // op.setModules(newOp.getModules());
+        // part.setOptimisationParams(op);
+        // partService.updatePart(part);
         return "optimise";
     }
 }
