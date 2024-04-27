1aaf312f95b3ef5b59b31a70740859c388478c8c
DylanJQ <47297648+Dylan-J-Q@users.noreply.github.com>
2024-04-27 13:44:56 +0100

    File execution
    The received File is now executing

--- python_backend/python_api.py
+++ python_backend/python_api.py
@@ -4,6 +4,7 @@ This is a API module that emulates the simulations that are used to optimise the
 
 from flask import Flask, request, jsonify
 from modules import module
+import subprocess
 
 app = Flask(__name__)
 
@@ -11,27 +12,39 @@ app = Flask(__name__)
 def test_string():
     """This function returns the string 'test String'."""
 
-    return module.string()
+    return module.string(), 200
 
 @app.route('/add', methods=['POST'])
 def add():
     """This function takes a JSON file and sums the values in the array in the file."""
 
     json = request.get_json()
-    return str(module.add(json['array']))
+    return str(module.add(json['array'])), 200
 
 @app.route('/optimise', methods=['POST'])
 def optimise():
     """Takes the JSON file and the Module and runs the module with the JSON file as input"""
 
     files = request.files.getlist('file')
-    print(request.files)
 
     if len(files) != 2:
         return jsonify("Incorrect number of files"), 400
-
     
-    return jsonify("ggs"), 200
+    python_file = files.pop()
+    json_file = files.pop()
+
+    python_file.save('python_file2.py')
+    json_file.save('json_file2.json')
+
+    try:
+        result = subprocess.run(["python", 'python_file2.py', 'json_file2.json'], 
+                                check=True,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
+                                text=True)
+        return jsonify(result.stdout), 200
+    except:
+        return jsonify("Error executing the module"), 500 
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
