76ea54f6278bfece0ce3f3d0597cd35c22a70090
georgecorfield <gacorfield50@gmail.com>
2024-05-01 21:59:45 +0100

    FIle and Part tests

--- src/test/java/UoBToolchainGroup/DistributedToolchainIntegration/controllerTests/ErrorControllerTest.java
+++ src/test/java/UoBToolchainGroup/DistributedToolchainIntegration/controllerTests/ErrorControllerTest.java
@@ -1,5 +1,22 @@
 package UoBToolchainGroup.DistributedToolchainIntegration.controllerTests;
 
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import UoBToolchainGroup.DistributedToolchainIntegration.controller.CustomErrorController;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(CustomErrorController.class)
+@TestInstance(Lifecycle.PER_CLASS)
 public class ErrorControllerTest {
+    
+    @Autowired
+    private MockMvc mvc;
+
     
 }
